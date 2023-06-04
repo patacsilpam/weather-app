4e26ea447f085b2900f208ca9181b637ea4185d9
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-06-05 00:02:23 +0800

    updated scripts

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
+let weatherData = {};
+let weatherIcon = document.querySelector('.img-temp');
 async function getWeatherData(location){
   const apiKey = "a6465cb74727863ed5577f7efc4f73b9";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
   try{
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    weatherData = {
+      temperature: data.main.temp,
+      condition: data.weather[0].main,
+      area: data.name,
+      wind: data.wind.speed,
+      humidity: data.main.humidity,
+      visibility: data.visibility,
+      icon:data.weather.icon
+    };
+    console.log(data)
+  
+    updateUI();
+    return weatherData;
   } catch(error){
-    console.log("error")
-    throw error
+    throw error;
   }
 }
-getWeatherData('Laoac');
+
+function updateUI(){
+ 
+  document.querySelector('.temperature').innerText = Math.round(weatherData.temperature);
+  document.querySelector('#uv-index').innerText = '5';
+  document.querySelector('#wind').innerText = weatherData.wind;
+  document.querySelector('#humidity').innerText = weatherData.humidity;
+  document.querySelector('#visibility').innerText = weatherData.visibility;
+  switch(weatherData.condition){
+    case 'Clouds':
+      weatherIcon.src = 'images/clouds.png';
+      break;
+    case 'Sunny':
+      weatherIcon.src = 'images/sunny.png';
+      break;
+    case 'Rain':
+      weatherIcon.src = 'images/rainy-day.png';
+      break;
+    case "Winds":
+      weatherIcon.src = 'images/windy.png';
+    default:
+      return weatherIcon.src = 'images/cloudy.png';
+  }
+}
+
+function searchLocation(){
+  const btnSubmit = document.getElementById('btn-submit');
+  btnSubmit.addEventListener("click",async function(){
+    const locID = document.getElementById('location').value; 
+    await getWeatherData(locID);
+  })
+}
+searchLocation();
+
